7d5eb24109b748e306b8dfd5a60c9553e9205308
Tahar <tntmeijs@gmail.com>
2020-08-08 19:47:44 +0200

    Basic search queries [ADD] Search results page. [ADD] Search query route. [CHANGE] Search query parameter name. [DELETE] Index router.

--- WebServer/app.js
+++ WebServer/app.js
@@ -1,9 +1,8 @@
 var express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -11,8 +10,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+/* GET home page */
+app.get('/', function(req, res, next) {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+/* GET search results */
+app.get('/search', function(req, res, next) {
+  console.log("Search query: " + req.query.search);
+  res.sendFile(path.join(__dirname, 'public', 'search_results.html'));
+});
+
+/* Set the static serving directory after the routes to ensure console.log()
+   works correctly. Logging to the console will no longer after this functino */
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
